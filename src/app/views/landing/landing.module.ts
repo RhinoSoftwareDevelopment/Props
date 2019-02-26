fe62b473207c4912adb5036bf2d9adea9176b2d9
Juan Pablo Peñaloza Botero <juan.pa097@gmail.com>
2019-02-26 14:57:55 -0500

    Finished landing front

--- src/app/views/landing/landing.module.ts
+++ src/app/views/landing/landing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialDesignModule } from '../material-design/material-design.module';
 import { LandingComponent } from './landing/landing.component';
+
 
 @NgModule({
   declarations: [LandingComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FlexLayoutModule,
+    MaterialDesignModule
+  ],
+  exports: [
+    LandingComponent
   ]
 })
 export class LandingModule { }
