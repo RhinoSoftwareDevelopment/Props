912470c79000b33e9149b0e5f0e19406589ebdf8
Juan Pablo Peñaloza Botero <juan.pa097@gmail.com>
2019-03-06 12:16:49 -0500

    Added articles service and displayed all articles in front end

---
+++ src/app/shared/article.model.ts
@@ -0,0 +1,7 @@
+export interface Article {
+    details?: string;
+    isAvailable: boolean;
+    name: string;
+    picture_path?: string;
+    warehouse_id: number; // TODO - Can be change to an enum.
+}
--- src/app/views/rent-prop/product-card/product-card.component.ts
+++ src/app/views/rent-prop/product-card/product-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Article } from 'src/app/shared/article.model';
 
 @Component({
   selector: 'app-product-card',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCardComponent implements OnInit {
 
+  @Input() article: Article;
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  /**
+   * Returns a string with the warehouse name.
+   * This method should be using an enum, or a 
+   * more elegant way, but for now is ok.
+   */
+  getWarehouseName():string {
+    switch (this.article.warehouse_id) {
+      case 1: return "Bodega aula múltiple";
+      case 2: return "Depósito 2";
+      case 3: return "Depósito 5";
+      case 4: return "Bodega camerino";
+      default: "Bodega no especificada";
+    }
   }
 
 }
